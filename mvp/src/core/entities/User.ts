5678c4126eecb3221cdd0f345f20a6d062657c2f
hyeonproject <axzswq@gmail.com>
2023-04-15 23:53:32 +0900

    add: User.ts

---
+++ mvp/src/core/entities/User.ts
@@ -0,0 +1,22 @@
+export class User {
+  id: string;
+  nickName: string;
+  password: string;
+  phone: number;
+  token: string;
+  createAt: string;
+
+  constructor(
+    id: string,
+    nickName: string,
+    password: string,
+    phone: number,
+    token: string,
+  ) {
+    this.id = id;
+    this.nickName = nickName;
+    this.password = password;
+    this.phone = phone;
+    this.token = token;
+  }
+}
