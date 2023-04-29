0dc7a96caff808ba1a016deee1de152707597884
hyeonproject <axzswq@gmail.com>
2023-04-30 00:31:41 +0900

    fix: 아키텍쳐 구현 변경하기

--- mvp/src/main.ts
+++ mvp/src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyCsrf from '@fastify/csrf-protection';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, {
+    cors: true,
+  });
+
+  await app.register(fastifyCsrf);
   await app.listen(3000);
 }
 bootstrap();
